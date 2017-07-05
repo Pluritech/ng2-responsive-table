9a7747f7c5a11529fc55dbace95ff1e4cd5f831e
mduraes1994 <mduraes1994@gmail.com>
2017-07-05 14:25:14 -0300

    Add support for checkbox column

--- src/table.component.ts
+++ src/table.component.ts
@@ -1,5 +1,5 @@
 import { TableContent } from './table.content';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ng-table',
@@ -9,10 +9,48 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TableComponent implements OnInit {
 
   @Input() data: TableContent;
+  @Output() selectedChange: EventEmitter<any> = new EventEmitter<any>();
+  @Output() selectedChangeAll: EventEmitter<any> = new EventEmitter<any>();
+  public selected: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle(row) {
+    if (row) {
+      const index = this.selected.indexOf(row);
+      if (index  === -1) {
+        this.selected.push(row);
+      } else {
+        this.selected.splice(index, 1);
+      }
+      this.selectedChange.emit(this.selected);
+      console.log(this.selected);
+    } else {
+      this.handleSelectAll();
+      this.selectedChangeAll.emit({
+        selecteds: this.selected,
+        allSelected: this.selected.length ? true : false
+      });
+      console.log({
+        selecteds: this.selected,
+        allSelected: this.selected.length ? true : false
+      });
+    }
+  }
+
+  exists(row) {
+    return this.selected.indexOf(row) > -1;
+  }
+
+  private handleSelectAll() {
+    if (this.selected.length) {
+      this.selected.splice(0, this.selected.length);
+    } else {
+      this.selected = this.selected.concat(this.data.rows);
+    }
   }
 
 }
--- src/table.content.ts
+++ src/table.content.ts
@@ -2,18 +2,23 @@ export interface TableContent {
     columns: TableColumn[];
     rows: any[];
     actions: TableButton[];
-    noRowsMsg?: string
+    noRowsMsg?: string;
 }
 
 export interface TableColumn {
     name: string;
     id: string;
+    checkbox?: TableCheckbox;
 }
 
 export interface TableButton {
     name: string;
     class: string;
     icon: string;
-    handler(row: any):void;
+    handler(row: any): void;
     condition?(row: any): boolean;
+}
+
+export interface TableCheckbox {
+    singleName?: string;
 }
